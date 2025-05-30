5e777aff2de20b06e087c6147699f9c7da52ede3
SophiaChirskaya <sophia.chirskaya@gmail.com>
2025-05-30 20:51:55 +0200

    [feat]api index & show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
     public function index() {
-        return "sei nella index API";
+
+
+        $projects = Project::with('type')->get();
+
+        return response()->json(
+            [
+                "success" => true,
+                "data" => $projects
+                ]
+            );
+    }
+
+    public function show(Project $project) {
+        
+        $project->load('type', 'technologies');
+
+        return response()->json(
+            [
+                "success" => true,
+                "data" => $project
+                ]
+            );
     }
 }
